2df2263a39fd1a1941f3d6fd74dd27f047575a0e
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-03-27 17:31:19 +0300

    Написали код

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Введите слова или числa через запятую ',' : ");
+String valueStr = Console.ReadLine(); // числа текстом
+string[] values = valueStr.Split(","); // числа по отдельности
+
+Console.WriteLine($"[{String.Join(";", values)}]");
+string[] result = new string[values.Length];
+for (int i = 0; i < values.Length; i++)
+{
+    if (values[i].Length < 4)
+    {
+       result[i] = values[i];
+    }
+   
+}
+result = result.Where(x => x != null).ToArray();
+
+
+Console.WriteLine($"[{String.Join(";", result)}]");
